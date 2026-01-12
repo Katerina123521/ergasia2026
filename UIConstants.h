6360ce7922683cb2ab21a8e3ffa1c483b3243c1f
Your Name <yourname@example.com>
2026-01-12 02:55:54 +0200

    add uUIConstants header

---
+++ UIConstants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+constexpr int WIN_W = 1024;
+constexpr int WIN_H = 768;
+
+constexpr float UI_LEFT_W = 260.0f;
+constexpr float UI_RIGHT_W = 260.0f;
+constexpr float UI_TOP_H = 64.0f;
+constexpr float UI_MARGIN = 14.0f;
+
